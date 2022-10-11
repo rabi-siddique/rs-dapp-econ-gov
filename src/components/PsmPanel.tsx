550d1d6c7a9d95fe042bdb98a363e79a2fdd2d33
Dan Connolly <connolly@agoric.com>
2022-10-11 14:00:26 -0500

    update anchors list with USDC from Axelar, Gravity

--- src/components/PsmPanel.tsx
+++ src/components/PsmPanel.tsx
@@ -14,7 +14,7 @@ import ProposePauseOffers from './ProposePauseOffers';
 import { OfferId } from './questions.js';
 
 // TODO fetch list from RPC
-const anchors = ['AUSD', 'USDT'];
+const anchors = ['AUSD', 'USDC_axl', 'USDC_grv'];
 
 interface Props {}
 
