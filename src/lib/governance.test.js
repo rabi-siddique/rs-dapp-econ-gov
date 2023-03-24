b9b4a6e48d2f8bdfa4acbb9a39da72c4f703b5b7
samsiegart <samsiegart@gmail.com>
2023-03-23 21:25:39 -0700

    fix: update tests

--- src/lib/governance.test.js
+++ src/lib/governance.test.js
@@ -3,18 +3,42 @@ import { expect, test } from 'vitest';
 import { outcomeMessage } from './governance';
 
 test('outcomeMessage', () => {
-  expect(outcomeMessage(undefined)).toBe('⏳ Vote Closes');
-  expect(outcomeMessage({})).toBe('???');
-  expect(outcomeMessage({ outcome: 'fail', reason: 'some reason' })).toBe(
-    '❌ some reason',
-  );
-  expect(outcomeMessage({ outcome: 'win', position: { noChange: {} } })).toBe(
-    '❌ Change Rejected',
-  );
-  expect(outcomeMessage({ outcome: 'win', position: { dontUpdate: {} } })).toBe(
-    '❌ Change Rejected',
-  );
-  expect(outcomeMessage({ outcome: 'win', position: { strings: [] } })).toBe(
-    '✅ Change Accepted',
-  );
+  expect(outcomeMessage(undefined)).toStrictEqual({
+    message: '⏳ Vote Closes',
+    color: 'bg-yellow-500 border border-yellow-100 bg-opacity-5',
+  });
+  expect(outcomeMessage({})).toStrictEqual({
+    message: '???',
+    color: 'bg-yellow-500 border border-yellow-100 bg-opacity-5',
+  });
+  expect(
+    outcomeMessage({ outcome: 'fail', reason: 'some reason' }),
+  ).toStrictEqual({
+    message: '❌ some reason',
+    color: 'bg-red-400 bg-opacity-5',
+  });
+  expect(
+    outcomeMessage({ outcome: 'win', position: { noChange: {} } }),
+  ).toStrictEqual({
+    message: '❌ Change Rejected',
+    color: 'bg-red-400 bg-opacity-5',
+  });
+  expect(
+    outcomeMessage({ outcome: 'win', position: { dontUpdate: {} } }),
+  ).toStrictEqual({
+    message: '❌ Change Rejected',
+    color: 'bg-red-400 bg-opacity-5',
+  });
+  expect(
+    outcomeMessage({ outcome: 'win', position: { dontInvoke: {} } }),
+  ).toStrictEqual({
+    message: '❌ Change Rejected',
+    color: 'bg-red-400 bg-opacity-5',
+  });
+  expect(
+    outcomeMessage({ outcome: 'win', position: { strings: [] } }),
+  ).toStrictEqual({
+    message: '✅ Change Accepted',
+    color: 'bg-green-400 bg-opacity-10',
+  });
 });
