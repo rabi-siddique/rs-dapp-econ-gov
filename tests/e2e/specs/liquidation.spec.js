e802578b0c306f84e50b1478ad599a6b5a2de6b5
rabi-siddique <rabisiddique27@gmail.com>
2024-04-04 07:24:28 +0500

    test: add test adjusting vault params

---
+++ tests/e2e/specs/liquidation.spec.js
@@ -0,0 +1,123 @@
+/* eslint-disable ui-testing/no-disabled-tests */
+describe('Liquidation Tests', () => {
+  let startTime;
+  context('Setting up accounts', () => {
+    it('should set up wallets for two members of the econ committee.', () => {
+      cy.setupWallet({
+        secretWords:
+          'purse park grow equip size away dismiss used evolve live blouse scorpion enjoy crunch combine day second news off crowd broken crop zoo subject',
+        walletName: 'gov2',
+      });
+      cy.setupWallet({
+        secretWords:
+          'tilt add stairs mandate extra wash choose fashion earth feature reopen until move lazy carbon pledge sure own comfort this nasty clap tower table',
+        walletName: 'gov1',
+      });
+    });
+
+    it('should connect with chain and wallet', () => {
+      cy.visit('/?agoricNet=local');
+      cy.acceptAccess();
+    });
+  });
+
+  context('Adjusting Vault Params', () => {
+    it('should allow gov1 to create a proposal', () => {
+      cy.visit('/?agoricNet=local');
+
+      cy.get('button').contains('Vaults').click();
+      cy.get('button').contains('Select Manager').click();
+      cy.get('button').contains('manager0').click();
+
+      cy.get('label')
+        .contains('LiquidationMargin')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('150');
+        });
+
+      cy.get('label')
+        .contains('LiquidationPadding')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('25');
+        });
+
+      cy.get('label')
+        .contains('LiquidationPenalty')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('1');
+        });
+
+      cy.get('label')
+        .contains('StabilityFee')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('1');
+        });
+
+      cy.get('label')
+        .contains('MintFee')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type('0.5');
+        });
+
+      cy.get('label')
+        .contains('Minutes until close of vote')
+        .parent()
+        .within(() => {
+          cy.get('input').clear().type(1);
+        });
+      cy.get('[value="Propose Parameter Change"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p')
+        .contains('sent')
+        .should('be.visible')
+        .then(() => {
+          startTime = Date.now();
+        });
+    });
+
+    it('should allow gov1 to vote on the proposal', () => {
+      cy.visit('/?agoricNet=local');
+
+      cy.get('button').contains('Vote').click();
+      cy.get('p').contains('YES').click();
+      cy.get('input:enabled[value="Submit Vote"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p').contains('sent').should('be.visible');
+    });
+
+    it('should allow gov2 to vote on the proposal', () => {
+      cy.switchWallet('gov2');
+      cy.visit('/?agoricNet=local');
+
+      cy.get('button').contains('Vote').click();
+      cy.get('p').contains('YES').click();
+      cy.get('input:enabled[value="Submit Vote"]').click();
+
+      cy.confirmTransaction();
+      cy.get('p').contains('sent').should('be.visible');
+    });
+
+    it('should wait for proposal to pass', () => {
+      cy.wait(60000 - Date.now() + startTime);
+      cy.visit('/?agoricNet=local');
+
+      cy.get('button').contains('History').click();
+
+      cy.get('code')
+        .contains('VaultFactory - ATOM')
+        .parent()
+        .parent()
+        .parent()
+        .within(() => {
+          cy.get('span').contains('Change Accepted').should('be.visible');
+        });
+    });
+  });
+});
