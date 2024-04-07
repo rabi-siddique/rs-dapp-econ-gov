eaf491ccdcf4d589050217ad910220736ce2e03e
rabi-siddique <rabisiddique27@gmail.com>
2024-04-07 23:03:02 +0500

    chore: adding gov1 and gov2 keys when setting up wallet for a3p:latest

--- tests/e2e/specs/tests.spec.js
+++ tests/e2e/specs/tests.spec.js
@@ -5,12 +5,12 @@ describe('Tests for creating proposals', () => {
     it('should set up wallets for two members of the econ committee.', () => {
       cy.setupWallet({
         secretWords:
-          'purse park grow equip size away dismiss used evolve live blouse scorpion enjoy crunch combine day second news off crowd broken crop zoo subject',
+          'such field health riot cost kitten silly tube flash wrap festival portion imitate this make question host bitter puppy wait area glide soldier knee',
         walletName: 'gov2',
       });
       cy.setupWallet({
         secretWords:
-          'tilt add stairs mandate extra wash choose fashion earth feature reopen until move lazy carbon pledge sure own comfort this nasty clap tower table',
+          'physical immune cargo feel crawl style fox require inhale law local glory cheese bring swear royal spy buyer diesel field when task spin alley',
         walletName: 'gov1',
       });
     });
